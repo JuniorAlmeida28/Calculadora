15b4a29039543517fea03d10c1452ca09d765b31
Junior Almeida <jrjunioralmeida@outlook.com>
2025-08-25 14:30:03 -0300

    Criação das classes Display, Botao, Teclado e configuração do posicionamento do display e teclado

---
+++ src/main/jave/br/com/calculado/view/Botao.java
@@ -0,0 +1,6 @@
+package main.jave.br.com.calculado.view;
+
+import javax.swing.*;
+
+public class Botao extends JButton {
+}
---
+++ src/main/jave/br/com/calculado/view/Display.java
@@ -0,0 +1,11 @@
+package main.jave.br.com.calculado.view;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Display extends JPanel {
+
+    public Display() {
+
+    }
+}
